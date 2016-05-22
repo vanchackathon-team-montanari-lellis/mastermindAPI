8d79e8923840d2984c56c58341d5b5b08e1fdebc
Lucas Montanari <falecomolucao@gmail.com>
2016-05-22 10:25:50 -0300

    Storing data in the cloud.

--- mastermind-api/src/main/java/com/vanhackathon/mastermind/configuration/ApplicationSwaggerConfig.java
+++ mastermind-api/src/main/java/com/vanhackathon/mastermind/configuration/ApplicationSwaggerConfig.java
@@ -2,6 +2,7 @@ package com.vanhackathon.mastermind.configuration;
 
 import org.springframework.context.annotation.Bean;
 
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -16,6 +17,17 @@ public class ApplicationSwaggerConfig {
 	public Docket docket() {
 		Docket docket = new Docket(DocumentationType.SWAGGER_2);
 
+		docket = docket.apiInfo(apiInfo());
+
 		return docket;
+	}
+
+	private ApiInfo apiInfo() {
+		ApiInfo apInfo = new ApiInfo("Mastermind API",
+				"This is the Mastermind backend server. You can find out more about Mastermind at https://en.wikipedia.org/wiki/Mastermind_(board_game).",
+				"1.0", "Mastermind API", "http://www.gnu.org/licenses/gpl-3.0.html",
+				"Bruno Lellis (brunolellis@gmail.com) Lucas Montanari (lucas_montanari@hotmail.com)",
+				"http://www.gnu.org/licenses/gpl-3.0.html");
+		return apInfo;
 	}
 }
